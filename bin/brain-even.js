84220b93e16df71c76c00fdf121afbfb8644bd4c
Berdnik <ggwpez1987@mail.ru>
2023-03-14 11:51:14 +0700

    brain-even test

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,12 +6,12 @@ import userName from './brain-meeting.js';
 
 const isEven = () => {
   const name = userName();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
   for (let userScore = 1; userScore < 4; userScore += 1) {
     const randomNumber = getRandomIntNumber(100);
-    const answer = readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
+    const answer = readlineSync.question(`Question: ${randomNumber}\nYour answer: \n`);
     if (!checkIsEven(randomNumber, answer)) {
-      console.log(`Let's try again, ${name}`);
+      console.log(`Let's try again, ${name}\n`);
       isEven(name);
       break;
     }
--- bin/brain-meeting.js
+++ bin/brain-meeting.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!\n');
 const userName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  const name = readlineSync.question('May I have your name? \n');
+  console.log(`Hello, ${name}\n`);
   return name;
 };
 
