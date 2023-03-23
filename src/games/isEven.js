bce4bdf9234485cf11b623031aa8967457454200
Berdnik <ggwpez1987@mail.ru>
2023-03-23 23:46:19 +0700

    isEven.js and isPrime.js fixed + progression.js optimized

--- src/games/isEven.js
+++ src/games/isEven.js
@@ -1,15 +1,15 @@
-import runGame from '../test.js';
+import runGame from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const checkIsEven = (number) => {
-  if (number % 2 === 0) return 'yes';
-  return 'no';
+  if (number % 2 === 0) return true;
+  return false;
 };
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 const getQuestionAndAnswer = () => {
   const randomNumber = getRandomNumber(0, 100);
   const question = `${randomNumber}`;
-  const correctAnswer = checkIsEven(randomNumber);
+  const correctAnswer = checkIsEven(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
--- src/games/progression.js
+++ src/games/progression.js
@@ -2,19 +2,30 @@ import runGame from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const description = 'What number is missing in the progression?';
-const getQuestionAndAnswer = () => {
+
+const createProgression = () => {
   const firstValue = getRandomNumber(0, 10);
   const arifmeticalDiff = getRandomNumber(1, 10);
   const lengthOfProgression = getRandomNumber(5, 12);
-  const finalProgression = [];
+  const progression = [];
   const lastValueOfProgression = firstValue + (lengthOfProgression - 1) * arifmeticalDiff;
-  const randomNumberOfProgression = getRandomNumber(0, lengthOfProgression);
-  let disturbedProgression = [];
   for (let i = firstValue; i <= lastValueOfProgression; i += arifmeticalDiff) {
-    finalProgression.push(i);
+    progression.push(i);
   }
+  return progression;
+};
+
+const disturbingProgression = (finalProgression, randomNumberOfProgression) => {
+  let disturbedProgression = [];
   disturbedProgression = [...finalProgression];
   disturbedProgression[randomNumberOfProgression] = '..';
+  return disturbedProgression;
+};
+
+const getQuestionAndAnswer = () => {
+  const finalProgression = createProgression();
+  const randomNumberOfProgression = getRandomNumber(0, finalProgression.length);
+  const disturbedProgression = disturbingProgression(finalProgression, randomNumberOfProgression);
   const stringProgression = disturbedProgression.join(' ');
   const question = `${stringProgression}`;
   const correctAnswer = String(finalProgression[randomNumberOfProgression]);
